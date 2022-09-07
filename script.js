8e9ec1809793329d048123f0c79c43487e0d13b8
Greg <greg19850@gmail.com>
2022-09-07 19:44:49 +0100

    created all necessary variables from DOM elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const balance = document.getElementById('balance');
+const income = document.getElementById('money-plus');
+const expense = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const transactionText = document.getElementById('text');
+const transactionAmount = document.getElementById('amount');
